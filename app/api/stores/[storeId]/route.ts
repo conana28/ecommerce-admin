fc0c67ac085cfcbd156ce43fbc0285f10a9eb78b
conana28 <33334673+conana28@users.noreply.github.com>
2023-07-04 21:34:59 +1200

    Finish settings up to billboard

---
+++ app/api/stores/[storeId]/route.ts
@@ -0,0 +1,74 @@
+import { NextResponse } from "next/server";
+import { auth } from "@clerk/nextjs";
+
+import prismadb from "@/lib/prismadb";
+
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+
+    const { name } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 403 });
+    }
+
+    if (!name) {
+      return new NextResponse("Name is required", { status: 400 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+    // Use deleteMany instead of deleteOne as can have multiple userIds 
+    const store = await prismadb.store.updateMany({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+      data: {
+        name
+      }
+    });
+  
+    return NextResponse.json(store);
+  } catch (error) {
+    console.log('[STORE_PATCH]', error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+};
+
+
+export async function DELETE(
+ _req: Request,  // even though don't use req, need to include it as a parameter as params must be 2nd parameter
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 403 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
+// Use deleteMany instead of deleteOne as can have multiple userIds 
+    const store = await prismadb.store.deleteMany({  
+      where: {
+        id: params.storeId,
+        userId
+      }
+    });
+  
+    return NextResponse.json(store);
+  } catch (error) {
+    console.log('[STORE_DELETE]', error);
+    return new NextResponse("Internal error", { status: 500 });
+  }
+};
---
+++ components/store-switcher.tsx
@@ -0,0 +1,126 @@
+"use client";
+
+import { useState } from "react";
+import { Store } from "@prisma/client";
+import { useParams, useRouter } from "next/navigation";
+import {
+  Check,
+  ChevronsUpDown,
+  PlusCircle,
+  Store as StoreIcon,
+} from "lucide-react";
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import {
+  Command,
+  CommandList,
+  CommandInput,
+  CommandEmpty,
+  CommandGroup,
+  CommandItem,
+  CommandSeparator,
+} from "./ui/command";
+
+type PopoverTriggerProps = React.ComponentPropsWithoutRef<
+  typeof PopoverTrigger
+>;
+
+interface StoreSwitcherProps extends PopoverTriggerProps {
+  // array of objects with store data
+  items: Store[];
+}
+
+export default function StoreSwitcher({
+  className,
+  items = [],
+}: StoreSwitcherProps) {
+  const storeModal = useStoreModal();
+  const params = useParams();
+  const router = useRouter();
+
+  const formattedItems = items.map((item) => ({
+    label: item.name,
+    value: item.id,
+  }));
+
+  // state to control the current popover state
+  const [open, setOpen] = useState(false);
+
+  // which is the current store?
+  const currentStore = formattedItems.find(
+    (item) => item.value === params.storeId
+  );
+
+  // what happens when we select a store?
+  const onStoreSelect = (store: { value: string; label: string }) => {
+    // close the popover
+    setOpen(false);
+    router.push(`/${store.value}`);
+  };
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          size="sm"
+          role="combobox"
+          className={cn("w-[200px] justify-between", className)}
+        >
+          <StoreIcon className="mr-2 h-4 w-4" />
+          {currentStore?.label || "Select a store"}
+          <ChevronsUpDown className="ml-auto h-4 w-4 shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-[200px] p-0">
+        <Command>
+          <CommandList>
+            <CommandInput placeholder="Search store" />
+            <CommandEmpty>No Store Found</CommandEmpty>
+            <CommandGroup heading="Stores">
+              {formattedItems.map((store) => (
+                <CommandItem
+                  key={store.value}
+                  onSelect={() => onStoreSelect(store)}
+                  className="text-sm"
+                >
+                  <StoreIcon className="mr-2 h-4 w-4" />
+                  {store.label}
+                  <Check
+                    className={cn(
+                      "ml-auto h-4 w-4",
+                      currentStore?.value === store.value
+                        ? "opacity-100"
+                        : "opacity-0"
+                    )}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+          <CommandSeparator />
+          <CommandList>
+            <CommandGroup>
+              <CommandItem
+                onSelect={() => {
+                  setOpen(false);
+                  storeModal.onOpen();
+                }}
+              >
+                <PlusCircle className="mr-2 h-5 w-5" />
+                Create Store
+              </CommandItem>
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  );
+}
